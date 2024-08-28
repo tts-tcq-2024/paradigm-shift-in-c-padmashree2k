bc39d9ec585de9c2ced093d321439f998694ccb2
padmashree2k <95606111+padmashree2k@users.noreply.github.com>
2024-08-28 21:27:33 +0530

    Update checker.c
    4th patch for assignment on Cyclomatic Complexity

--- checker.c
+++ checker.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 
+int is_Within_Range(float value, float minimum, float maximum) {
+  return (value >= minimum && value <= maximum);
+}
+
 int check_scope(float value, float minimum, float maximum, const char* errorMessage) {
   if (!is_Within_Range(value, minimum, maximum)) {
     printf("%s\n", errorMessage);
@@ -9,11 +13,6 @@ int check_scope(float value, float minimum, float maximum, const char* errorMess
   return 1;
 }
 
-int is_Within_Range(float value, float minimum, float maximum) {
-  return (value >= minimum && value <= maximum);
-}
-
-// Function that checks if battery parameters are within their respective ranges
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   int Tremp_Ok = check_scope(temperature, 0, 45, "The temperature is out of range...");
   int Soc_Ok = check_scope(soc, 20, 80, "The state of Charge is out of range...");
